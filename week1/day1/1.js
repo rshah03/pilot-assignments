c3bd45bd8aecdd46dc1995bb9b25518cd5a09712
Aaron Dancer <me@aarondancer.com>
2016-08-30 16:05:24 -0500

    Removed bad solution

--- week1/day1/1.js
+++ week1/day1/1.js
@@ -1,7 +1,5 @@
 var console = require('./lib/consolelog.js');
 
 module.exports = function triangle() {
-  for (var i = 1; i <= 7; i++) {
-    console.log('#'.repeat(i))
-  }
+  // write your code here
 }
